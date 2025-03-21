99d579f2881e936ddd8e1da60b885a12fdbff9ba
Vlad Nechaev <nechaevvlad06@gmail.com>
2025-03-22 01:08:11 +0300

    update

--- bot.py
+++ bot.py
@@ -8,8 +8,12 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def welcome_new_members(message):
     for new_member in message.new_chat_members:
         first_name = new_member.first_name
-        welcome_message = f'Добро пожаловать, {first_name}! Пожалуйста представь себя в разделе #Осебе'
-        bot.send_message(message.chat.id, welcome_message)
+        welcome_message = (
+            f"Добро пожаловать, {first_name}! "
+            "Пожалуйста представь себя в разделе "
+            "<a href='https://t.me/c/2559274539/56'>О себе</a>"
+        )
+        bot.send_message(message.chat.id, welcome_message, parse_mode="HTML")
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
